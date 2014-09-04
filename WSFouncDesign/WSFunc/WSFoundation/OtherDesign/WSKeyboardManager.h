d66aa7279aecf5703f1279884267bc98fb955807
wangsen <578595193@qq.com>
2014-09-04 17:29:18 +0800

    小修改

--- WSFouncDesign/WSFunc/WSFoundation/OtherDesign/WSKeyboardManager.h
+++ WSFouncDesign/WSFunc/WSFoundation/OtherDesign/WSKeyboardManager.h
@@ -1,13 +1,57 @@
 //
 //  DKKeyboardManager.h
-//  DaniateKit
+//  WSFouncDesign
 //
 //  Created by Daniate on 14/8/2.
-//  Copyright (c) 2014年 Daniate. All rights reserved.
+//  Copyright (c) 2014年 wangsen. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
-
+// 严格单例
+#if __has_feature(objc_arc)
+#define kWSStrictSingletonForClass(__CLASS_NAME__) \
+static __CLASS_NAME__ *shared##__CLASS_NAME__ = nil;\
++ (__CLASS_NAME__ *)shared##__CLASS_NAME__ {\
+static dispatch_once_t onceToken;\
+dispatch_once(&onceToken, ^{\
+shared##__CLASS_NAME__ = [[super allocWithZone:NULL] init];\
+});\
+return shared##__CLASS_NAME__;\
+}\
++ (id)allocWithZone:(NSZone *)zone {\
+return [__CLASS_NAME__ shared##__CLASS_NAME__];\
+}\
+- (id)copyWithZone:(NSZone *)zone {\
+return self;\
+}
+#else
+#define kWSStrictSingletonForClass(__CLASS_NAME__) \
+static __CLASS_NAME__ *shared##__CLASS_NAME__ = nil;\
++ (__CLASS_NAME__ *)shared##__CLASS_NAME__ {\
+static dispatch_once_t onceToken;\
+dispatch_once(&onceToken, ^{\
+shared##__CLASS_NAME__ = [[super allocWithZone:NULL] init];\
+});\
+return shared##__CLASS_NAME__;\
+}\
++ (id)allocWithZone:(NSZone *)zone {\
+return [__CLASS_NAME__ shared##__CLASS_NAME__];\
+}\
+- (id)copyWithZone:(NSZone *)zone {\
+return self;\
+}\
+- (id)retain {\
+return self;\
+}\
+- (NSUInteger)retainCount {\
+return NSUIntegerMax;\
+}\
+- (oneway void)release {\
+}\
+- (id)autorelease {\
+return self;\
+}
+#endif
 @interface WSKeyboardManager : NSObject
 
 @property (nonatomic, assign) UIScrollView *currentScrollView;
