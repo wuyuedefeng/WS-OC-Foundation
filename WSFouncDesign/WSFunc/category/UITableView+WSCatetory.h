409e4e9a1b46ed402f1be56520d58e1b6a355054
wangsen <wangs@tongxinyiliao.com>
2014-10-18 09:48:30 +0800

    添加  uitableview分类

---
+++ WSFouncDesign/WSFunc/category/UITableView+WSCatetory.h
@@ -0,0 +1,13 @@
+//
+//  UITableView+WSCatetory.h
+//  WSFouncDesign
+//
+//  Created by TX-009 on 14-10-17.
+//  Copyright (c) 2014年 TX-009. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UITableView (WSCatetory)
+
+@end
---
+++ WSFouncDesign/WSFunc/category/UITableView+WSCatetory.m
@@ -0,0 +1,13 @@
+//
+//  UITableView+WSCatetory.m
+//  WSFouncDesign
+//
+//  Created by TX-009 on 14-10-17.
+//  Copyright (c) 2014年 TX-009. All rights reserved.
+//
+
+#import "UITableView+WSCatetory.h"
+
+@implementation UITableView (WSCatetory)
+
+@end
