d466077c9ca985b1501028f4aabf3b5f0b003a13
wangsen <wangs@tongxinyiliao.com>
2014-10-31 17:58:33 +0800

    。。

---
+++ WSFouncDesign/WSFunc/WSFoundation/PhonoBrowser/PhotoBrowserViewController.h
@@ -0,0 +1,13 @@
+//
+//  PhotoBrowserViewController.h
+//  WSFouncDesign
+//
+//  Created by TX-009 on 14-10-31.
+//  Copyright (c) 2014年 TX-009. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PhotoBrowserViewController : UIViewController
+
+@end
